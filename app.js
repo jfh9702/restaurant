e1acd66d972ca17f0b21f91e1c144532ed5c339b
jfh9702 <jfh9702@gmail.com>
2022-06-19 22:58:12 +0800

    restaurant

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+const restaurantsData = require("./restaurant.json").results
+
+const app = express()
+const port = 3000
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }))
+app.set("view engine", "handlebars")
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+  res.render("index", { restaurantsData })
+})
+
+app.get("/search", (req, res) => {
+  if (!req.query.keywords) {
+    return res.redirect("/")
+  }
+
+  const keywords = req.query.keywords
+  const keyword = req.query.keywords.trim().toLowerCase()
+
+  const filterRestaurantsData = restaurantsData.filter(
+    data =>
+      data.name.toLowerCase().includes(keyword) ||
+      data.category.includes(keyword)
+  )
+
+  res.render("index", { restaurantsData: filterRestaurantsData, keywords })
+})
+
+app.get("/restaurants/:restaurantId", (req, res) => {
+  const { restaurantId } = req.params
+  const restaurantData = restaurantsData.find(
+    data => data.id === Number(restaurantId)
+    // 也可使用 e.id === +restaurantId
+  )
+  res.render("show", { restaurantData })
+})
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`)
+})
